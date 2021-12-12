441da3da3aa156240cd30e4e0fd25d56328126e8
Scott Kerkvliet <scott_kerkvliet@hotmail.com>
2021-12-12 16:58:53 -0500

    Day 12: Part 1

--- day12/part1.go
+++ day12/part1.go
@@ -6,12 +6,43 @@ import (
 	"log"
 )
 
+func pathsToEnd(cave *fileReader.Cave, visited map[string]bool) (numPaths int) {
+	if cave == nil || visited == nil {
+		return
+	}
+	if cave.Name == "end" {
+		return 1
+	}
+	if !cave.Big {
+		if _, hasBeenVisited := visited[cave.Name]; hasBeenVisited {
+			return
+		}
+		visited[cave.Name] = true
+	}
+
+	for _, neighbour := range cave.Connections {
+		numPaths += pathsToEnd(neighbour, visited)
+	}
+
+	if !cave.Big {
+		delete(visited, cave.Name)
+	}
+
+	return
+}
+
+func printNumPaths(start *fileReader.Cave) {
+	visitedCaves := map[string]bool{}
+	numPaths := pathsToEnd(start, visitedCaves)
+
+	fmt.Printf("There are %v paths to end.\n", numPaths)
+}
+
 func main() {
 	startCave, err := fileReader.ReadCaves("caves.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Start cave has key \"%v\". Is big? %v\n", startCave.Name, startCave.Big)
-	fmt.Printf("Start cave has %v connections.\n", len(startCave.Connections))
+	printNumPaths(startCave)
 }
